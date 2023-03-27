3a8abd2b7178facb61e5e37f70af8286fd6eea45
vuggy17 <vuggy17@gmail.com>
2023-03-27 22:28:52 +0700

    chore: use typings

--- packages/externals/nodert.streams/index.js
+++ packages/externals/nodert.streams/index.js
@@ -11,53 +11,49 @@ var Readable = require('stream').Readable;
 var Writable = require('stream').Writable;
 var util = require('util');
 var bufferUtils = require('./build/Release/NodeRT_Buffer_Utils.node');
-var streams = require('@nodert-win11-22h2/windows.storage.streams');
+var streams = require('windows.storage.streams');
 
 util.inherits(InputStream, Readable);
 util.inherits(OutputStream, Writable);
 
 function InputStream(source, options) {
-  if (!(this instanceof InputStream))
-    return new InputStream(source, options);
+  if (!(this instanceof InputStream)) return new InputStream(source, options);
 
   Readable.call(this, options);
 
-  options = options || { highWaterMark: 16 * 1024 };
+  options = options || {highWaterMark: 16 * 1024};
 
   // source is a winrt iinputstream, such as a socket or file
   this._source = source;
   this._buffer = new Buffer(options.highWaterMark);
 }
 
-InputStream.prototype._read = function(n) {
+InputStream.prototype._read = function (n) {
   var self = this;
   var ibuffer = bufferUtils.toIBuffer(this._buffer);
 
   try {
-    this._source.readAsync(ibuffer, n, 1, function(err, ibufferFinal) {
+    this._source.readAsync(ibuffer, n, 1, function (err, ibufferFinal) {
       if (err) {
         console.error('error reading: ', err);
         return self.emit('error', err);
-      };
+      }
       if (ibufferFinal && ibufferFinal.length > 0) {
         var target = new Buffer(ibufferFinal.length);
         self._buffer.copy(target, 0, 0, ibufferFinal.length);
         self.push(target);
-      }
-      else if (ibufferFinal.length < n) {
+      } else if (ibufferFinal.length < n) {
         // signal that the stream had ended
         self.push(null);
       }
     });
-  }
-  catch (e) {
+  } catch (e) {
     self.emit('error', e);
   }
 };
 
 function OutputStream(source, options) {
-  if (!(this instanceof OutputStream))
-    return new OutputStream(source, options);
+  if (!(this instanceof OutputStream)) return new OutputStream(source, options);
 
   Writable.call(this, options);
 
@@ -67,19 +63,18 @@ function OutputStream(source, options) {
   var self = this;
 }
 
-OutputStream.prototype._write = function(chunk, encoding, callback) {
+OutputStream.prototype._write = function (chunk, encoding, callback) {
   var self = this;
 
   try {
-    this._source.writeAsync(bufferUtils.toIBuffer(chunk), function(err, bytesWritten) {
+    this._source.writeAsync(bufferUtils.toIBuffer(chunk), function (err, bytesWritten) {
       if (err) return callback(err);
 
-      self._source.flushAsync(function(err) {
+      self._source.flushAsync(function (err) {
         return callback(err, bytesWritten);
       });
     });
-  }
-  catch (e) {
+  } catch (e) {
     self.emit('error', e);
   }
 };
