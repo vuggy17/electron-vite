5e2bc45c0fba848c72a8233af1535e5ec38ca823
vuggy17 <vuggy17@gmail.com>
2023-04-07 17:58:48 +0700

    update build jobs, add linting

---
+++ test/fake.test.ts
@@ -0,0 +1,8 @@
+import {describe, expect, test} from 'vitest';
+
+describe('Fake component test', () => {
+  test('Should show title', () => {
+    const shouldTrue = true;
+    expect(shouldTrue).toBe(true);
+  });
+});
